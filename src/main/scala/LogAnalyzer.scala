4c86fdcdddc88a58d89fbd3c9533573a1a9a9610
Issa Fikadu <memiwo@gmail.com>
2016-07-12 00:07:25 -0500

    Implement Analysis of site access by hour of the day

--- src/main/scala/LogAnalyzer.scala
+++ src/main/scala/LogAnalyzer.scala
@@ -1,6 +1,7 @@
 import edu.mum.log.Log
 import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs.{FileSystem, Path}
+import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
 
 /**
@@ -11,24 +12,48 @@ object LogAnalyzer {
 
   def main(args: Array[String]) {
 
-    val outputDir = new Path("/bigdata/analytics/output")
 
     val sc = new SparkContext(new SparkConf().setAppName("Apache Log Analyzer").setMaster("local"))
 
     val conf = new Configuration()
-   /* val fs = FileSystem.get(conf)
+    val fs = FileSystem.get(conf)
 
-    fs.delete(outputDir)*/
-    val input = sc.textFile("/bigdata/analytics/input/access_log.txt")
+    val paths = Util.prepareFileSystem(conf)
 
-    input.map(s => Log(s)).foreach(l => println(" ******************************************"+l))
-    /*val totalLog = input.count()
-    println("Total logs : "+totalLog)*/
+    val outputDir = new Path(paths(1))
 
-    /*val output = sc.parallelize(Seq(result))
-    output.saveAsTextFile("/bigdata/project/spark/output")*/
+    fs.delete(outputDir)
+
+    val input = sc.textFile(paths(0))
+
+    input.cache()
+
+    accessByHour(input,sc)
+
     sc.stop
 
   }
+
+  def accessByHour(input:RDD[String], sc:SparkContext) = {
+    val output = input.
+      filter(r => !r.isEmpty)
+      .map(s => Log(s))
+      .map(log => (log.dateTime.getHour, 1))
+      .reduceByKey(_+_)
+      .map{ case (k,v) => (v,k)}
+      .sortByKey(ascending = false)
+      .map{ case (v,k) => k + "," + v}
+
+    val header = sc.parallelize(Array("hour,count"))
+    val finalOutput = header.union(output)
+    //Coalesce is used to group together the output from different partition in to one
+    finalOutput.coalesce(1, true).saveAsTextFile("/bigdata/analytics/output")
+   /* finalOutput.repartition(1).saveAsTextFile("/bigdata/analytics/output")*/
+  }
+
+  def accessByIpAddress(input:RDD[String], sc:SparkContext)  = {
+
+  }
+
 
 }
