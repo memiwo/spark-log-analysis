7c15f1ad1f92cf0d2093a81d666451c34f55f64e
Issa Fikadu <memiwo@gmail.com>
2016-07-12 00:26:51 -0500

    Implement Analysis of site access by Ip Address

--- src/main/scala/LogAnalyzer.scala
+++ src/main/scala/LogAnalyzer.scala
@@ -26,18 +26,19 @@ object LogAnalyzer {
 
     val input = sc.textFile(paths(0))
 
-    input.cache()
+    input.filter(r => !r.isEmpty).cache()
 
     accessByHour(input,sc)
+    accessByIpAddress(input, sc)
 
     sc.stop
 
   }
 
   def accessByHour(input:RDD[String], sc:SparkContext) = {
-    val output = input.
-      filter(r => !r.isEmpty)
+    val output = input
       .map(s => Log(s))
+        .filter(log => log != null)
       .map(log => (log.dateTime.getHour, 1))
       .reduceByKey(_+_)
       .map{ case (k,v) => (v,k)}
@@ -47,12 +48,25 @@ object LogAnalyzer {
     val header = sc.parallelize(Array("hour,count"))
     val finalOutput = header.union(output)
     //Coalesce is used to group together the output from different partition in to one
-    finalOutput.coalesce(1, true).saveAsTextFile("/bigdata/analytics/output")
+    finalOutput.coalesce(1, true).saveAsTextFile("/bigdata/analytics/output/byHour")
    /* finalOutput.repartition(1).saveAsTextFile("/bigdata/analytics/output")*/
   }
 
   def accessByIpAddress(input:RDD[String], sc:SparkContext)  = {
+    val output = input
+        .map(s => Log(s))
+        .filter(l => l != null)
+        .map(log => (log.ipAddress, 1))
+        .reduceByKey(_+_)
+        .map{ case (k,v) => (v,k)}
+        .sortByKey(ascending = false)
+        .map{ case (v,k) => k + "," + v}
 
+    val header = sc.parallelize(Array("ipAddress,count"))
+    val finalOutput = header.union(output)
+    //Coalesce is used to group together the output from different partition in to one
+    finalOutput.coalesce(1, true).saveAsTextFile("/bigdata/analytics/output/byIpAddress")
+    /* finalOutput.repartition(1).saveAsTextFile("/bigdata/analytics/output")*/
   }
 
 
